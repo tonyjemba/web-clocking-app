5c559d6be3ed0fee11940f4d90c291f6fa152624
Tony Jemba <jjembatony@gmail.com>
2022-12-08 00:11:48 +0300

    edit user

--- resources/js/components/UserTable.vue
+++ resources/js/components/UserTable.vue
@@ -19,10 +19,8 @@
                     <td>admin</td>
                     <td>Sun, 23/Oct/2016</td>
                     <td>
-                        <div class="d-flex flex-row">
+                        <div >
                             <button type="button" class="btn btn-danger bl">Delete</button>
-                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">Edit</button>
                         </div>
                     </td>
 
@@ -34,10 +32,8 @@
                     <td>admin</td>
                     <td>Sun, 23/Oct/2016</td>
                     <td>
-                        <div class="d-flex flex-row">
+                        <div >
                             <button type="button" class="btn btn-danger bl">Delete</button>
-                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">Edit</button>
                         </div>
                     </td>
 
@@ -45,47 +41,7 @@
 
             </tbody>
         </table>
-        <!-- Edit User Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="exampleModalLabel">Edit User</h4>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label for="name" class="form-label fw-bold">Name</label>
-                            <input type="text" class="form-control form-control-lg" id="name"
-                                placeholder="Full Name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label fw-bold">Email address</label>
-                            <input type="email" class="form-control form-control-lg" id="exampleInputEmail1"
-                                aria-describedby="emailHelp" placeholder="email">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label fw-bold">Password</label>
-                            <input type="password" class="form-control form-control-lg" id="exampleInputPassword1"
-                                placeholder="Password">
-                        </div>
-                        <div class="d-flex flex-row ">
-                            <div>
-                                <input type="radio" id="html" name="role" value="HTML">
-                                <label for="html" class="lef">Admin</label>
-                            </div>
-                            <div class=" space">
-                                <input type="radio" id="html" name="role" value="HTML" checked>
-                                <label for="html" class="lef">User</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer w-100  d-flex justify-content-start">
-                        <div><button type="button" class="btn btn-primary">Save </button></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
 </template>
 <style>
--- resources/js/pages/DashboardPage.vue
+++ resources/js/pages/DashboardPage.vue
@@ -3,10 +3,11 @@ import Navigation from '../components/Navigation.vue';
 import TimeSection from '../components/TimeSection.vue';
 import ReportSection from '../components/ReportSection.vue';
 import UserSection from '../components/UserSection.vue';
+import EditUserSection from '../components/EditUserSection.vue';
 </script>
 <template>
     <Navigation />
-    <UserSection/>
+    <EditUserSection/>
 
 </template>
 
