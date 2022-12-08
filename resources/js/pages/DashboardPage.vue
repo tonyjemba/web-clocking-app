5fe8440a21ce747c7e1d869f02996e98f0cea079
Tony Jemba <jjembatony@gmail.com>
2022-12-08 22:07:30 +0300

    conditionaly rendering dashboard sections using pinia store

--- resources/js/pages/DashboardPage.vue
+++ resources/js/pages/DashboardPage.vue
@@ -6,13 +6,17 @@ import UserSection from './components/UserSection.vue';
 import { useDashboardSectionsStore } from '../Store/dasboardSections';
 //const props = defineProps(['registeredTimeIn'])
 //console.log(props.registeredTimeIn)
+
+//accessing store for conditionally rendering dashboard sections
 const store = useDashboardSectionsStore();
+
+//props with email and type of autenticated user
 const props = defineProps(['email','type']);
 </script>
 <template>
     <Navigation  :email="props.email" :type="props.type"/>
     
-    <!-- conditionally rendering dashboard sections -->
+    <!-- conditionally rendering dashboard sections using Pinia -->
 
      <TimeSection v-if="(store.TimeSection === true)" />
      <ReportSection v-else-if="(store.ReportSection === true)"/>
